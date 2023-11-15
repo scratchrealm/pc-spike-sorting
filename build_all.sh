dbda7a4825282ae41efec59fa771b98d89328cd7
Jeremy Magland <jmagland@flatironinstitute.org>
2023-11-15 09:51:58 -0500

    ks3 spec

--- build_all.sh
+++ build_all.sh
@@ -3,31 +3,31 @@
 set -ex
 
 cd dandi_upload
+dendro make-app-spec-file --app-dir . --spec-output-file spec.json
 docker build . -t magland/pc-dandi-upload
 docker push magland/pc-dandi-upload
-dendro make-app-spec-file --app-dir . --spec-output-file spec.json
 cd ..
 
 cd kilosort2_5
+dendro make-app-spec-file --app-dir . --spec-output-file spec.json
 docker build . -t magland/pc-kilosort2_5
 docker push magland/pc-kilosort2_5
-dendro make-app-spec-file --app-dir . --spec-output-file spec.json
 cd ..
 
 cd kilosort3
+dendro make-app-spec-file --app-dir . --spec-output-file spec.json
 docker build . -t magland/pc-kilosort3
 docker push magland/pc-kilosort3
-dendro make-app-spec-file --app-dir . --spec-output-file spec.json
 cd ..
 
 cd mountainsort5
+dendro make-app-spec-file --app-dir . --spec-output-file spec.json
 docker build . -t magland/pc-mountainsort5
 docker push magland/pc-mountainsort5
-dendro make-app-spec-file --app-dir . --spec-output-file spec.json
 cd ..
 
 cd spike_sorting_utils
+dendro make-app-spec-file --app-dir . --spec-output-file spec.json
 docker build . -t magland/pc-spike-sorting-utils
 docker push magland/pc-spike-sorting-utils
-dendro make-app-spec-file --app-dir . --spec-output-file spec.json
 cd ..
